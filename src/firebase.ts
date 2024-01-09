f213dee4f08c41f7bf286280d54019b1af2b3bf8
성지원 <jiwon.sung@vtw.co.kr>
2024-01-09 16:39:13 +0900

    firebase initialize

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrUu2xkQ_FAWt-EA_BjBmhmpzvt814DYA",
+  authDomain: "nwitter-reloaded-24ada.firebaseapp.com",
+  projectId: "nwitter-reloaded-24ada",
+  storageBucket: "nwitter-reloaded-24ada.appspot.com",
+  messagingSenderId: "625468274821",
+  appId: "1:625468274821:web:763703f89f13cb7e8ad1ec"
+};
+
+const app = initializeApp(firebaseConfig);
