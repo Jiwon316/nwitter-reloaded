0fe9efa5703c0cdfc9cabddd1e53f88866921fa3
성지원 <jiwon.sung@vtw.co.kr>
2024-01-12 13:18:33 +0900

    github 로그인 추가

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -4,6 +4,7 @@ import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+import GitgubButton from "../components/github-btn";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -71,6 +72,7 @@ export default function Login() {
       <Switcher>
         Don't have an account? <Link to="/create-account">Create one &rarr;</Link>
       </Switcher>
+      <GitgubButton/>
     </Wrapper>
   );
 }
