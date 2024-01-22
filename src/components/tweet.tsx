98d3f53bae041cca010058b63098c0740059143c
성지원 <jiwon.sung@vtw.co.kr>
2024-01-22 16:32:18 +0900

    deploy 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -11,26 +12,55 @@ const Wrapper = styled.div`
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-radius: 15px;
 `;
-const Column = styled.div``;
+const Column = styled.div`
+  &:last-child {
+    place-self: end;
+  }
+`;
 const Username = styled.span`
   font-weight: 600;
-  font: 15px;
+  font-size: 15px;
 `;
 const Payload = styled.p`
-  margin: 10px 0px;
+  margin: 30px 0px;
   font-size: 18px;
   border-radius: 15px;
 `;
+const EditTweet = styled.textarea`
+  background-color: black;
+  width: 80%;
+  margin: 30px 10px;
+  font-size: 18px;
+  resize: none;
+  border: none;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &:focus {
+    outline: none;
+  }
+`;
 const Photo = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 150px;
+  height: 150px;
 `;
 
+const EditButton = styled.button`
+  color: white;
+  font-weight: 600;
+  background-color: black;
+  border: 1px solid tomato;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 const DeleteButton = styled.button`
   background-color: tomato;
   color: white;
   font-weight: 600;
-  border: 0;
+  border: 1px solid tomato;
   font-size: 12px;
   padding: 5px 10px;
   text-transform: uppercase;
@@ -38,30 +68,96 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
+const SaveButton = styled.button`
+  color: white;
+  font-weight: 600;
+  background-color: black;
+  border: 1px solid tomato;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
-  const onDelete = async() =>{
+  const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
-    if(!ok || user?.uid !== userId) return;
+    if (!ok || user?.uid !== userId) return;
     try {
-        await deleteDoc(doc(db,"tweets",id));
-        if(photo){
-            const photoRef = ref(storage,`tweets/${user.uid}-${user.displayName}/${id}`);
-            await deleteObject(photoRef);
-        }
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${id}`
+        );
+        await deleteObject(photoRef);
+      }
     } catch (error) {
-        console.log(error);
-    }finally{
-
+      console.log(error);
+    } finally {
+      //
     }
-  }
+  };
+  const [edit, setEdit] = useState(false);
+  const onEdit = async () => {
+    if (user?.uid !== userId) return;
+    try {
+      setEdit(true);
+    } catch (error) {}
+  };
+  const [edittedTweet, setTweet] = useState(tweet);
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(event.target.value);
+  };
+  const onSave = async () => {
+    if (!edittedTweet) return;
+    try {
+      await updateDoc(doc(db, "tweets", id), {
+        tweet: edittedTweet,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setEdit(false);
+    }
+  };
   return (
     <Wrapper>
-      <Column>
-        <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
-        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
-      </Column>
+      {edit ? (
+        <Column>
+          <EditTweet
+            required
+            rows={3}
+            maxLength={180}
+            value={edittedTweet}
+            onChange={onChange}
+          ></EditTweet>
+          <ButtonContainer>
+            <SaveButton onClick={onSave}>Save</SaveButton>
+          </ButtonContainer>
+        </Column>
+      ) : (
+        <Column>
+          <Username>{username}</Username>
+          <Payload>{tweet}</Payload>
+          <ButtonContainer>
+          {user?.uid === userId ? (
+            <EditButton onClick={onEdit}>Edit</EditButton>
+          ) : null}
+          {user?.uid === userId ? (
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          ) : null}
+          </ButtonContainer>
+        </Column>
+      )}
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
   );
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,5 +1,196 @@
-export default function Profile(){
-    return(
-        <h1>Profile</h1>
-    )
+import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50px;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+  svg {
+    width: 20px;
+    margin: 0px 10px;
+    cursor: pointer;
+    vertical-align: middle;
+  }
+`;
+const Tweets = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const EditUserName = styled.input`
+  font-size: 20px;
+  width: 100px;
+  background-color: black;
+  color: white;
+
+`;
+export default function Profile() {
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(
+        storage,
+        `users/${user.uid}-${user.displayName}/avatar`
+      );
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { photo, tweet, userId, username, createdAt } = doc.data();
+      return {
+        photo,
+        tweet,
+        userId,
+        username,
+        createdAt,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  const [editName, setEditName] = useState(false);
+  const [edittedName, setName] = useState(user?.displayName || "Anonymous");
+  const onUsernameEdit = () => {
+    setEditName(true);
+  };
+  const onUernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const onUsernameSave = async()=>{
+    if(!user || !edittedName)return;
+    try {
+      await updateProfile(user,{
+        displayName: edittedName,
+      });  
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setEditName(false);
+    }
+  }
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        id="avatar"
+        type="file"
+        accept="image/*"
+        onChange={onAvatarChange}
+      />
+      {editName ? (
+        <Name>
+          <EditUserName
+            required
+            type="text"
+            value={edittedName}
+            onChange={onUernameChange}
+          ></EditUserName>
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            onClick={onUsernameSave}
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M3 3.5A1.5 1.5 0 0 1 4.5 2h6.879a1.5 1.5 0 0 1 1.06.44l4.122 4.12A1.5 1.5 0 0 1 17 7.622V16.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 3 16.5v-13Zm10.857 5.691a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 0 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+            />
+          </svg>
+        </Name>
+      ) : (
+        <Name>
+          {user?.displayName ? user.displayName : "Anonymous"}
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            onClick={onUsernameEdit}
+          >
+            <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+          </svg>
+        </Name>
+      )}
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
+    </Wrapper>
+  );
 }
